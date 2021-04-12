415e3f5b7e07703b423c93ce1a4dfb8a518d5760
JAIVERANDRESAND <>
2021-04-12 15:18:44 +0000

    Quinto

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+  int f=1;
+  for(int i=1;i<=n;i++)
+    f*=i;//f=f*i
+  return f;
+}
+
+int main() {
+  cout<<factorial(5);
+  return 0;
+}
