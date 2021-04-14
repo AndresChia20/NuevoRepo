ae7e0036d5b45e41534ef695383435f19a967b5c
JAIVERANDRESAND <>
2021-04-14 16:59:22 +0000

    Primer Java

---
+++ Programa1/Carro.java
@@ -0,0 +1,9 @@
+public class Carro{
+  private static String Color;
+  private static String Marca;
+  private static String Modelo;
+  
+  public static void main (String[] args){
+    System.out.println("Franchesco");
+  }
+}
